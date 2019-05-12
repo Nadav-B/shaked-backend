f3bfc1a1145cb05c07c32831e39e61bfb6cc99e1
Nadavbabai <nadavbabai@hotmail.com>
2019-05-12 13:46:04 +0200

    new files

---
+++ src/main/java/com/shakedimportservicebackend/shakedimportservice/ShakedImportServiceApplication.java
@@ -0,0 +1,13 @@
+package com.shakedimportservicebackend.shakedimportservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShakedImportServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShakedImportServiceApplication.class, args);
+	}
+
+}
