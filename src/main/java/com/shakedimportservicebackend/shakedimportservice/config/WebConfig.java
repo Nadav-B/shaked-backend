713dcf61fd2a13827d5c2fe65765612998e7f3eb
Nadavbabai <nadavbabai@hotmail.com>
2019-12-21 22:11:55 +0200

    file size

--- src/main/java/com/shakedimportservicebackend/shakedimportservice/config/WebConfig.java
+++ src/main/java/com/shakedimportservicebackend/shakedimportservice/config/WebConfig.java
@@ -1,9 +1,6 @@
 package com.shakedimportservicebackend.shakedimportservice.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.multipart.MultipartResolver;
-import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,11 +14,5 @@ public class WebConfig implements WebMvcConfigurer {
 
     }
 
-    @Bean
-    public MultipartResolver multipartResolver() {
-        CommonsMultipartResolver multipartResolver
-                = new CommonsMultipartResolver();
-        multipartResolver.setMaxUploadSize(100829040);
-        return multipartResolver;
-    }
+
 }
