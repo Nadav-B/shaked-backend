52f3c29809b1602cc323a9b78eb8c28a5d448157
Nadavbabai <nadavbabai@hotmail.com>
2020-02-11 22:47:30 +0100

    update spring version

--- src/main/java/com/shakedimportservicebackend/shakedimportservice/config/WebSecurityConfig.java
+++ src/main/java/com/shakedimportservicebackend/shakedimportservice/config/WebSecurityConfig.java
@@ -9,6 +9,8 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Slf4j
 @Configuration
@@ -55,7 +57,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth)
             throws Exception {
-        auth.inMemoryAuthentication().withUser(username).password("{noop}" + password);
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        auth.inMemoryAuthentication()
+                .withUser(username)
+                .password(encoder.encode(password))
+                .roles("ADMIN");
     }
 }
 
