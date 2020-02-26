b0962cfbc61b977218b85aa91c2eb7b7757f162c
Nadavbabai <nadavbabai@hotmail.com>
2020-02-26 23:19:28 +0100

    add for local

---
+++ src/main/java/com/shakedimportservicebackend/shakedimportservice/config/WebConfig.java
@@ -0,0 +1,21 @@
+/*
+package com.shakedimportservicebackend.shakedimportservice.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+// FOR LOCAL
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
+
+ */
