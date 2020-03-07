edd8031812428f920f1201927d9071f9a8efdd58
Nadavbabai <nadavbabai@hotmail.com>
2020-03-07 13:37:43 +0100

    [Edit] mysql

--- src/main/java/com/shakedimportservicebackend/shakedimportservice/config/WebSecurityConfig.java
+++ src/main/java/com/shakedimportservicebackend/shakedimportservice/config/WebSecurityConfig.java
@@ -26,6 +26,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
                 http
+                    //    .cors().and() // local
                         .csrf().disable()
                         .authorizeRequests()
 
