42c11aeee3a695925cd8f2b0791aa630365a7a42
Nadav Babai <nadavbabai@hotmail.com>
2020-07-28 23:15:09 +0200

    Revert "[edit] trying"
    This reverts commit 783f71564090bd8747de4eb12680acc3d163d668.

--- src/main/java/com/shakedimportservicebackend/shakedimportservice/persistence/model/Survey.java
+++ src/main/java/com/shakedimportservicebackend/shakedimportservice/persistence/model/Survey.java
@@ -31,6 +31,7 @@ public class Survey   {
 
     @OneToMany(mappedBy="survey",cascade = CascadeType.ALL)
     @JsonManagedReference
+    @Builder.Default
     private List<Answer> answers = new ArrayList<>() ;
 
     /*
