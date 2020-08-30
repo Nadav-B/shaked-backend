a897132e28a295ce658e65649e1c3a68231827e5
Nadav Babai <nadavbabai@hotmail.com>
2020-08-30 11:35:27 +0200

    fix(WebSecurityConfig) enabling cross origin for facebook

--- src/main/java/com/shakedimportservicebackend/shakedimportservice/config/WebSecurityConfig.java
+++ src/main/java/com/shakedimportservicebackend/shakedimportservice/config/WebSecurityConfig.java
@@ -28,6 +28,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
         http
+                .cors()
+                .and()
                 .csrf()
                 .disable()
                 .authorizeRequests()
