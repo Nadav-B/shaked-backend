c4339447ffbaab3459467a8e648b86f63abefe24
Nadav Babai <nadavbabai@hotmail.com>
2020-08-30 12:59:03 +0200

    fix(Cors) disable

--- src/main/java/com/shakedimportservicebackend/shakedimportservice/config/WebSecurityConfig.java
+++ src/main/java/com/shakedimportservicebackend/shakedimportservice/config/WebSecurityConfig.java
@@ -28,8 +28,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
         http
-                .cors()
-                .and()
                 .csrf()
                 .disable()
                 .authorizeRequests()
