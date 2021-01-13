6f22d238995118c8561f352a6ab3e41dca33512a
nbabai <nbabai@3pc.de>
2021-01-13 13:05:50 +0100

    Update SecurityConstants.java

--- src/main/java/com/shakedimportservicebackend/shakedimportservice/configuration/SecurityConstants.java
+++ src/main/java/com/shakedimportservicebackend/shakedimportservice/configuration/SecurityConstants.java
@@ -3,7 +3,7 @@ package com.shakedimportservicebackend.shakedimportservice.configuration;
 public class SecurityConstants {
 
     public static final String SIGN_UP_URL = "/users/record";
-    public static final String KEY = "qx3t6w9z$C&F)J@NcQfTjWnZr4u7x!A%D*G-KaPdSgUkXp2s5v8y/B?E(H+MbQeTh";
+    public static final String KEY = "eyJhbGciOiJIUzUxMiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTYxMDUzOTQ1OCwiaWF0IjoxNjEwNTM5NDU4fQ.Y8MadTEH3G300wpHSUT-3_UFxo8kLfcxFuIZ1UPwBMSx-foN6OkkNxBmJBXg48hkOWdFlG2jp3rQcLGbs-2WAg";
     public static final String HEADER_NAME = "Authorization";
     public static final Long EXPIRATION_TIME = 1000L*60*30;
 }
