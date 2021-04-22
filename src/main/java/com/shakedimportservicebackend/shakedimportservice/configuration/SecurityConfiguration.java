d68fa77e4ec1016636cea5bb3d7e509b53a35589
nbabai <nadavbabai@hotmail.com>
2021-04-22 18:11:55 +0200

    clean code

--- src/main/java/com/shakedimportservicebackend/shakedimportservice/configuration/SecurityConfiguration.java
+++ src/main/java/com/shakedimportservicebackend/shakedimportservice/configuration/SecurityConfiguration.java
@@ -52,13 +52,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         final CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("*"));
-        // setAllowCredentials(true) is important, otherwise:
-        // The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request's credentials mode is 'include'.
-        // configuration.setAllowCredentials(true);
-        // setAllowedHeaders is important! Without it, OPTIONS preflight request
-        // will fail with 403 Invalid CORS request
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
-        // allow header "Location" to be read by clients to enable them to read the location of an uploaded group logo
         configuration.addExposedHeader("Token");
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
