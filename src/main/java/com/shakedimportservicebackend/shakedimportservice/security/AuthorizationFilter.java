43944ed229720fc4ba48c3c44923ef34548968db
nbabai <nadavbabai@hotmail.com>
2021-05-23 15:23:38 +0200

    [Edit] spring security

--- src/main/java/com/shakedimportservicebackend/shakedimportservice/security/AuthorizationFilter.java
+++ src/main/java/com/shakedimportservicebackend/shakedimportservice/security/AuthorizationFilter.java
@@ -1,23 +1,21 @@
 package com.shakedimportservicebackend.shakedimportservice.security;
 
 import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.security.Keys;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import java.io.IOException;
 import java.util.ArrayList;
 
-import static com.shakedimportservicebackend.shakedimportservice.configuration.SecurityConstants.HEADER_NAME;
-import static com.shakedimportservicebackend.shakedimportservice.configuration.SecurityConstants.KEY;
-import static com.shakedimportservicebackend.shakedimportservice.configuration.SecurityConstants.TOKEN_PREFIX;
+import static com.shakedimportservicebackend.shakedimportservice.configuration.SecurityConstants.*;
 
 
 public class AuthorizationFilter extends BasicAuthenticationFilter {
