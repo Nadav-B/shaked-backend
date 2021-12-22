c4728e8007cb8258ac209dd5c32c28a270b52352
nbabai <nbabai@3pc.de>
2021-12-22 21:10:12 +0100

    cors

--- src/main/java/com/shakedimportservicebackend/shakedimportservice/resolvers/ServiceResolver.java
+++ src/main/java/com/shakedimportservicebackend/shakedimportservice/resolvers/ServiceResolver.java
@@ -6,6 +6,7 @@ import io.leangen.graphql.annotations.GraphQLNonNull;
 import io.leangen.graphql.annotations.GraphQLQuery;
 import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 import java.util.Optional;
@@ -26,6 +27,7 @@ public class ServiceResolver {
     }
 
     @GraphQLQuery(name = "getServices")
+    @CrossOrigin
     public List<Service> getServices() {
         return serviceRepository.findAll();
     }
