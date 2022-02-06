c3ca0556abae32c53ceb3d9ec6710b424d5839ae
nbabai <nadavbabai@hotmail.com>
2022-02-06 16:44:49 +0100

    Revert "Revert "Merge branch 'develop' of github.com:nana1001/shaked-backend into develop""
    This reverts commit 6b46cccac26b0bbfb1a898e96008bd1e24ed793d.

---
+++ src/main/java/com/shakedimportservicebackend/shakedimportservice/resolvers/ServiceResolver.java
@@ -0,0 +1,34 @@
+package com.shakedimportservicebackend.shakedimportservice.resolvers;
+
+import com.shakedimportservicebackend.shakedimportservice.models.Service;
+import com.shakedimportservicebackend.shakedimportservice.repositories.ServiceRepository;
+import io.leangen.graphql.annotations.GraphQLNonNull;
+import io.leangen.graphql.annotations.GraphQLQuery;
+import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+@GraphQLApi
+public class ServiceResolver {
+
+    private final ServiceRepository serviceRepository;
+
+    public ServiceResolver(ServiceRepository serviceRepository) {
+        this.serviceRepository = serviceRepository;
+    }
+
+    @GraphQLQuery
+    public Optional<Service> getService(@GraphQLNonNull final Long id) {
+        return serviceRepository.findById(id);
+    }
+
+    @GraphQLQuery(name = "getServices")
+    @CrossOrigin
+    public List<Service> getServices() {
+        return serviceRepository.findAll();
+    }
+}
