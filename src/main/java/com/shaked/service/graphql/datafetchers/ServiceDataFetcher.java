e6341449dc525d729f3f6d3ce33a4e0d750d76d1
nbabai <nbabai@3pc.de>
2022-03-11 14:22:53 +0100

    Update ServiceDataFetcher.java

--- src/main/java/com/shaked/service/graphql/datafetchers/ServiceDataFetcher.java
+++ src/main/java/com/shaked/service/graphql/datafetchers/ServiceDataFetcher.java
@@ -6,9 +6,7 @@ import com.netflix.graphql.dgs.DgsDataFetchingEnvironment;
 import com.netflix.graphql.dgs.DgsQuery;
 import com.netflix.graphql.dgs.InputArgument;
 import com.shaked.service.graphql.dataloaders.ServiceDataLoader;
-import com.shaked.service.graphql.dataloaders.TextDataLoader;
 import com.shaked.service.models.Service;
-import com.shaked.service.models.TextContainer;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -16,8 +14,8 @@ import java.util.concurrent.CompletableFuture;
 @DgsComponent
 public class ServiceDataFetcher {
     @DgsQuery(field = "service")
-    public CompletableFuture<Service> getService(@InputArgument String id, DgsDataFetchingEnvironment env) {
-        var dataLoader = env.<String, Service>getDataLoader(ServiceDataLoader.class);
+    public CompletableFuture<Service> getService(@InputArgument Long id, DgsDataFetchingEnvironment env) {
+        var dataLoader = env.<Long, Service>getDataLoader(ServiceDataLoader.class);
         return dataLoader.load(id);
     }
 }
