b5aa93c223967357a8208c54f89674ec2b5abaa9
nbabai <nadavbabai@hotmail.com>
2022-04-12 21:08:21 +0200

    init

--- src/main/java/com/shaked/service/configuration/SecurityConfiguration.java
+++ src/main/java/com/shaked/service/configuration/SecurityConfiguration.java
@@ -2,68 +2,30 @@
 
 package com.shaked.service.configuration;
 
-import com.shaked.service.security.AuthenticationFilter;
-import com.shaked.service.security.AuthorizationFilter;
-import com.shaked.service.resolvers.ApplicationUserDetailsService;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 
+@Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-    private ApplicationUserDetailsService userDetailsService;
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
-
-    public SecurityConfiguration(ApplicationUserDetailsService userDetailsService, BCryptPasswordEncoder bCryptPasswordEncoder) {
-        this.userDetailsService = userDetailsService;
-        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
-    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable().authorizeRequests()
-
-                .antMatchers("/graphql").permitAll()
-                .antMatchers("/actuator/**").permitAll()
-                .antMatchers("/articles/article/**").permitAll()
-                .antMatchers(HttpMethod.GET, "/services/service/**").permitAll()
-                .antMatchers(HttpMethod.GET, "/offers/offer/**").permitAll()
-                .antMatchers(HttpMethod.POST, "/contacts/post").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .addFilter(new AuthenticationFilter(authenticationManager()))
-                .addFilter(new AuthorizationFilter(authenticationManager()))
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.authorizeRequests()
+                .anyRequest().authenticated().and().httpBasic();
     }
 
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        final CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*"));
-        configuration.setAllowedMethods(Arrays.asList("*"));
-        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
-        configuration.addExposedHeader("Token");
-        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth, PasswordEncoder passwordEncoder) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("shai").password(passwordEncoder.encode("shai")).roles("ADMIN");
     }
-
-    @Override
-    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
-    }
-
-
 }
--- src/main/java/com/shaked/service/graphql/datafetchers/ArticlesDataFetcher.java
+++ src/main/java/com/shaked/service/graphql/datafetchers/ArticlesDataFetcher.java
@@ -2,18 +2,12 @@ package com.shaked.service.graphql.datafetchers;
 
 
 import com.netflix.graphql.dgs.DgsComponent;
-import com.netflix.graphql.dgs.DgsDataFetchingEnvironment;
 import com.netflix.graphql.dgs.DgsQuery;
-import com.netflix.graphql.dgs.InputArgument;
 import com.shaked.service.models.Article;
 import com.shaked.service.repositories.ArticleRepository;
+import org.springframework.security.access.annotation.Secured;
 
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CompletionStage;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
 
 
 @DgsComponent
@@ -26,6 +20,7 @@ public class ArticlesDataFetcher {
     }
 
     @DgsQuery(field = "articles")
+    @Secured("ROLE_ADMIN")
     public List<Article> getArticles() {
         return repository.findAll().stream().toList();
 
